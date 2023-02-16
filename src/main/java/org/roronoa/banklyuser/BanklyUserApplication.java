7aea53427f32f1539722e3ff1da585801caeb70b
Oussama Souayri <souayri.oussama@gmail.com>
2023-02-16 21:54:36 +0100

    first commit

---
+++ src/main/java/org/roronoa/banklyuser/BanklyUserApplication.java
@@ -0,0 +1,13 @@
+package org.roronoa.banklyuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanklyUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanklyUserApplication.class, args);
+    }
+
+}
